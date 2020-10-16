ae21916453b4cf22b2a3164a1d427c1e1c00099a
etsec-cloud <etienne.secondini@hotmail.fr>
2020-10-16 11:23:32 +0200

    fin

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var bouton = document.querySelector("#bouton");
+var bouton2 = document.querySelector("#bouton2");
 
 bouton.addEventListener("click", function() {
   document.getElementById("premier").classList.toggle("prout");
   document.getElementById("second").classList.toggle("prout");
-  //   document.querySelector(".navbar").classList.toggle("prout");
+  document.querySelector(".navbar").classList.toggle("prout");
+});
 
-  console.log("cheh");
+bouton2.addEventListener("click", function() {
+  document.getElementById("evole").classList.toggle("prout");
+  document.getElementById("planters").classList.toggle("prout");
+
+  // document.querySelector(".navbar").classList.toggle("prout");
 });
